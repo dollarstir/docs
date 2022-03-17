ab3fa89dfac7c1404f4b7ca4faa6070337e5ea83
dollarstir <kpin463@gmail.com>
2022-03-17 23:57:06 +0000

    sdfds

--- main/main.php
+++ main/main.php
@@ -1377,7 +1377,7 @@
             </div>
         </div>
     </div> -->
-
+    <web-particles id="tsparticles1" options='{"fps_limit":60,"interactivity":{"detectsOn":"canvas","events":{"onClick":{"enable":true,"mode":"push"},"onHover":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"push":{"particles_nb":4},"repulse":{"distance":200,"duration":0.4}}},"particles":{"color":{"value":"#ffffff"},"links":{"color":"#ffffff","distance":150,"enable":true,"opacity":0.4,"width":1},"move":{"bounce":false,"direction":"none","enable":true,"outMode":"out","random":false,"speed":2,"straight":false},"number":{"density":{"enable":true,"area":800},"value":80},"opacity":{"value":0.5},"shape":{"type":"circle"},"size":{"random":true,"value":5}},"detectRetina":true}'></web-particles>
     <footer class="relative pt-12 ">
         <div class="max-w-screen-2xl mx-auto w-full px-5">
             <div>
