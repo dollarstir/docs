71bf08dc3e5cb7a0bd1be0eb1e62a1ac5d11e539
dollarstir <kpin463@gmail.com>
2022-03-17 08:30:44 +0000

    working on particles

--- main/main.php
+++ main/main.php
@@ -224,26 +224,10 @@
                     </svg>
                 </div>
                 <div class="absolute bottom-0 right-6 -translate-y-20 pointer-events-none md:right-[12%]">
-                    <svg x-data="{
-        initializeAnimation: false,
-        init() {
-            setTimeout(() => {
-                this.initializeAnimation = true;
-            }, 1000);
-        },
-    }" :class="initializeAnimation ? 'animate-cube' : ''" class="text-red-600" width="46" height="53"
-                        viewBox="0 0 46 53" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="m23.102 1 22.1 12.704v25.404M23.101 1l-22.1 12.704v25.404" stroke="currentColor"
-                            stroke-width="1.435" stroke-linejoin="bevel" />
-                        <path d="m45.202 39.105-22.1 12.702L1 39.105" stroke="currentColor" stroke-width="1.435"
-                            stroke-linejoin="bevel" />
-                        <path transform="matrix(.86698 .49834 .00003 1 1 13.699)" stroke="currentColor"
-                            stroke-width="1.435" stroke-linejoin="bevel" d="M0 0h25.491v25.405H0z" />
-                        <path transform="matrix(.86698 -.49834 -.00003 1 23.102 26.402)" stroke="currentColor"
-                            stroke-width="1.435" stroke-linejoin="bevel" d="M0 0h25.491v25.405H0z" />
-                        <path transform="matrix(.86701 -.49829 .86701 .49829 1 13.702)" stroke="currentColor"
-                            stroke-width="1.435" stroke-linejoin="bevel" d="M0 0h25.491v25.491H0z" />
-                    </svg>
+                <svg  width="46" height="53">
+                    <ellipse cx="200" cy="80" rx="100" ry="50" style="fill:yellow;stroke:purple;stroke-width:1" />
+                    Sorry, your browser does not support inline SVG.  
+                </svg>
                 </div>
                 <div class="relative">
                     <h1 class="max-w-3xl mx-auto text-5xl font-bold text-center md:text-6xl lg:text-7xl">State Of The 
@@ -260,8 +244,8 @@
                                 Get Started
                             </span>
                         </a>
-                        <a class="group relative inline-flex border  focus:outline-none w-full sm:w-auto"
-                            href="https://youytuy.com" target="_blank">
+                        <a class="group relative inline-flex border border-red-600 focus:outline-none w-full sm:w-auto"
+                            href="https://laracasts.com" target="_blank">
                             <span
                                 class="w-full inline-flex items-center justify-center self-stretch px-4 py-2 text-sm text-red-600 text-center font-bold uppercase bg-white ring-1 ring-red-600 ring-offset-1 transform transition-transform group-hover:-translate-y-1 group-hover:-translate-x-1 group-focus:-translate-y-1 group-focus:-translate-x-1">
                                 Watch Tutorial
