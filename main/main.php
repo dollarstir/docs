2ef0a59adc216d3d70f1bfb63ed231e84153fc49
dollarstir <kpin463@gmail.com>
2022-03-17 07:07:04 +0000

    back again

--- main/main.php
+++ main/main.php
@@ -256,15 +256,15 @@
                         <a class="group relative inline-flex border border-red-500 focus:outline-none w-full sm:w-auto"
                             href="docs/9.x.html">
                             <span
-                                class="w-full inline-flex items-center justify-center self-stretch px-4 py-2 text-sm text-white text-center font-bold uppercase bg-red-500 ring-1 ring-red-500 ring-offset-1 ring-offset-red-500 transform transition-transform group-hover:-translate-y-1 group-hover:-translate-x-1 group-focus:-translate-y-1 group-focus:-translate-x-1">
+                                class="w-full inline-flex items-center justify-center self-stretch px-4 py-2 text-sm text-white text-center font-bold uppercase bg-red-500 ring-1  ring-offset-1 ring-offset-red-500 transform transition-transform">
                                 Get Started
                             </span>
                         </a>
-                        <a class="group relative inline-flex border border-red-600 focus:outline-none w-full sm:w-auto"
-                            href="https://laracasts.com" target="_blank">
+                        <a class="group relative inline-flex border  focus:outline-none w-full sm:w-auto"
+                            href="https://youytuy.com" target="_blank">
                             <span
                                 class="w-full inline-flex items-center justify-center self-stretch px-4 py-2 text-sm text-red-600 text-center font-bold uppercase bg-white ring-1 ring-red-600 ring-offset-1 transform transition-transform group-hover:-translate-y-1 group-hover:-translate-x-1 group-focus:-translate-y-1 group-focus:-translate-x-1">
-                                Watch Laracasts
+                                Watch Tutorial
                             </span>
                         </a>
                     </div>
