f3bbd2934be5d013630872c6d325ef5ad3ae79fc
dollarstir <kpin463@gmail.com>
2022-03-18 10:34:39 +0000

    dsfdf

--- main/team.php
+++ main/team.php
@@ -1,17 +1,8 @@
 <?php
-YolkUI::run(new Wrapper(
-    el::html('',
-    [
-        el::head([meta::viewport(),
-        el::title('York Framework'),import('css'),
-        ]),
-        el::body([
-            el::p('',"Send money with yolkpay"),
-            Yolkpay::paybutton(),
-            yolkpay::pay('Dollar','Stir','example@gmail.com','0556676471',
-            '10','ASDDSD2322'),
-            import('js'),
-            yolkpay::handler(),
-        ])
-    ])
-));
+ $send = new Yolksms();
+$send->sms("PhP Yolk",'0556676471,
+0233334343,
+044454545',
+"THis is Demo Message from yolk");
+
+
