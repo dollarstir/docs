5c3a901d40228c6a5512f83ce906f2f060613752
dollarstir <kpin463@gmail.com>
2022-03-18 16:28:20 +0000

    dsf

--- main/docs/9.x/installation.php
+++ main/docs/9.x/installation.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <?php head('');?>
+    <?php head('installation');?>
 
     <script>
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
