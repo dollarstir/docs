9caf5077959cba714f4c41b0c5d20d842ce1b678
dollarstir <kpin463@gmail.com>
2022-03-19 13:40:46 +0000

    ddssd

--- processor/processor.php
+++ processor/processor.php
@@ -12,6 +12,11 @@ if (isset($_GET['action'])) {
 
             $response = $s->basic('users',$search,['name',]);
 
+            if($response ==[]){
+                echo '<div  x-cloak class="mt-8 pb-32">
+                <div> No matching result</div>
+            </div>';
+            }
 
             foreach ($response as $row){
                echo ' <a class="search-result -mx-2 block p-3 text-gray-400 transition-colors duration-200 focus:outline-none focus:bg-dark-800 focus:text-gray-200 hover:text-gray-200" href="'.$row['email'].'">
