55ca6fb2c98b97c718cb91e00f0225c432b9bc0a
dollarstir <kpin463@gmail.com>
2022-03-19 13:51:57 +0000

    sdsdsd

--- processor/processor.php
+++ processor/processor.php
@@ -10,28 +10,31 @@ if (isset($_GET['action'])) {
             $s = new Search();
 
 
-            $response = $s->basic('users',$search,['name',]);
+            $response = $s->basic('docs',$search,['title','sub']);
 
             if($response ==[]){
                 echo '<div  x-cloak class="mt-8 pb-32">
                 <div> No matching result</div>
             </div>';
             }
-
-            foreach ($response as $row){
-               echo ' <a class="search-result -mx-2 block p-3 text-gray-400 transition-colors duration-200 focus:outline-none focus:bg-dark-800 focus:text-gray-200 hover:text-gray-200" href="'.$row['email'].'">
-               <div class="text-sm font-medium" >'.$row['name'].'</div>
-               <div class="mt-2">
-                   <div class="text-sm">
-                       <span class="text-red-600 opacity-75">#</span> <span>'.$row['name'].'</span>
-                   </div>
-
-
-
-                   
-               </div>
-           </a>';
+            else{
+                foreach ($response as $row){
+                    echo ' <a class="search-result -mx-2 block p-3 text-gray-400 transition-colors duration-200 focus:outline-none focus:bg-dark-800 focus:text-gray-200 hover:text-gray-200" href="'.$row['link'].'">
+                    <div class="text-sm font-medium" >'.$row['title'].'</div>
+                    <div class="mt-2">
+                        <div class="text-sm">
+                            <span class="text-red-600 opacity-75"><></span> <span>'.$row['sub'].'</span>
+                        </div>
+     
+     
+     
+                        
+                    </div>
+                </a>';
+                 }
             }
+
+            
 
             break;
         
