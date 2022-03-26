c7c3caa7c8ed93d292fecfc4bbaee7b8e62575c0
dollarstir <kpin463@gmail.com>
2022-03-26 22:49:10 +0000

    rergrg

--- yolk_create.php
+++ yolk_create.php
@@ -3,7 +3,7 @@
 $app_name = $context['appname'];
 
 echo '
-    curl https://download2283.mediafire.com/l73n04i8n1dg/iskdmemccdx8ctb/Archive.zip --output social.zip &&
+    curl https://phpyolk.com/clidownload --output social.zip &&
     unzip social.zip -d '.$app_name.' &&
     rm -f social.zip
 ';
