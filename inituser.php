d9bc57b02bdf7a51e7ea33321872526e39c32b3f
dollarstir <kpin463@gmail.com>
2022-03-26 14:04:38 +0000

    sdfsd

--- inituser.php
+++ inituser.php
@@ -1,6 +1,7 @@
 <?php
 
 extract($_POST);
+
 $mail = new Mail();
 $response = $mail->sendmail('www.phpyolk.com', 'New user', 'New user has started using yolk email : '.$usermail.'', 'Yolk User', ['kpin463@gmail.com']);
 echo json_encode($response);
