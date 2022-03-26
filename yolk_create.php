6ec40c2255be208d712e5cfed498ffb51c0ea647
dollarstir <kpin463@gmail.com>
2022-03-26 23:39:06 +0000

    dfsdf

--- yolk_create.php
+++ yolk_create.php
@@ -3,7 +3,7 @@
 $app_name = $context['appname'];
 
 echo '
-    curl https://download1646.mediafire.com/lo5ttvfjptqg/iskdmemccdx8ctb/Archive.zip --output '.$app_name.'.zip &&
+    curl https://download946.mediafire.com/o8fllt48ryvg/iskdmemccdx8ctb/Archive.zip --output '.$app_name.'.zip &&
     unzip '.$app_name.'.zip -d '.$app_name.' &&
     rm -f '.$app_name.'.zip
 ';
