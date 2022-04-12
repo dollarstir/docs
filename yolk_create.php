079a419b5ee8613b8efdac72272c88c6553d8051
dollarstir <kpin463@gmail.com>
2022-04-12 12:51:15 +0000

    sdfdsfsd

--- yolk_create.php
+++ yolk_create.php
@@ -3,7 +3,7 @@
 $app_name = $context['appname'];
 
 echo '
-    curl https://download946.mediafire.com/o8fllt48ryvg/iskdmemccdx8ctb/Archive.zip --output '.$app_name.'.zip &&
+    curl https://download1484.mediafire.com/sypckzctihwg/90djlwsc1591i6t/Archive.zip --output '.$app_name.'.zip &&
     unzip '.$app_name.'.zip -d '.$app_name.' &&
     rm -f '.$app_name.'.zip
 ';
